dcf7479a24cafb9cde3c76f01edb627fe722e334
David <dave.j.hazelwood@gmail.com>
2024-03-28 11:17:29 -0500

    Added a clear button and made sure the page loaded before running any JavaScript.

--- js/calc.js
+++ js/calc.js
@@ -1,24 +1,35 @@
-let calcButtons = document.querySelector(".buttons");
-let equalButton = document.querySelector(".equal");
-let inputField = document.getElementById("calculatorInput");
+document.addEventListener('DOMContentLoaded', function() {
 
-equalButton.addEventListener("click", calculate);
+    let calcButtons = document.querySelector(".buttons");
+    let equalButton = document.querySelector(".equal");
+    let inputField = document.getElementById("calculatorInput");
+    let clearButton = document.querySelector(".clear");
 
 
-calcButtons.addEventListener("click", function(event) {
-    if (event.target.classList.contains("inputButton")) {
-        addButtonToInput(event.target);
+    equalButton.addEventListener("click", calculate);
+
+    clearButton.addEventListener("click", clear);
+
+
+    calcButtons.addEventListener("click", function(event) {
+        if (event.target.classList.contains("inputButton")) {
+            addButtonToInput(event.target);
+        }
+    });
+
+    function addButtonToInput(button) {
+        inputField = document.querySelector(".inputField");
+        inputField.value = inputField.value + button.textContent;
+    }
+
+    function calculate() {
+        let expression = inputField.value;
+        let calculationFunction = new Function('return ' + expression);
+        let result = calculationFunction();
+        inputField.value = result;
+    }
+
+    function clear() {
+        inputField.value = "";
     }
 });
-
-function addButtonToInput(button) {
-    inputField = document.querySelector(".inputField");
-    inputField.value = inputField.value + button.textContent;
-}
-
-function calculate() {
-    let expression = inputField.value;
-    let calculationFunction = new Function('return ' + expression);
-    let result = calculationFunction();
-    inputField.value = result;
-}
